1802d4599df8bf8ee29c57640193f83b81b2b65b
Tarmo <n4378@student.jamk.fi>
2021-08-09 23:16:47 +0300

    Add card component, edit index page to use it

--- components/card.js
+++ components/card.js
@@ -1,5 +1,5 @@
 
-function Card({ children, altName, imgSrc }){
+export default function Card({ name, imgSrc, altName, tech, description, code, demo  }){
   return(
       <div className='w-11/12 sm:w-11/12 grid justify-center items-center m-auto'>
         <div>
@@ -10,13 +10,37 @@ function Card({ children, altName, imgSrc }){
           />
           <div className='relative px-4 -mt-16  '>
             <div className='bg-white p-6 rounded-lg shadow-lg'>
-              {children}
+              <h4 className='mt-1 text-xl font-semibold uppercase leading-tight truncate'>
+                {name}
+              </h4>
+
+              <div className='mt-2'>
+                {tech.map(techName => (
+                    <span className='bg-gray-300 rounded-full mr-2 px-3 py-0.5 font-semibold'>
+                        {techName}
+                      </span>
+                ))}
+              </div>
+              <div className='mt-4'>
+                {description}
+              </div>
+              <div className='mt-4'>
+                <a
+                    className='border py-2 px-4 bg-gray-700 shadow text-white rounded-full hover:bg-gray-600 transition duration-100'
+                    href={code}
+                >
+                  Code
+                </a>
+                <a
+                    href={demo}
+                    className='border py-2 px-4 ml-4 bg-gray-700 text-white rounded-full hover:bg-gray-600 transition duration-100'
+                >
+                  Demo
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
   )
-
 }
-
-export default Card
